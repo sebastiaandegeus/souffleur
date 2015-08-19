31eb800f3ffb61a051a7f687b1c0e16d50830b62
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-08-19 14:44:44 +0200

    fix mistake in name spec

--- spec/address/address_germany_spec.rb
+++ spec/address/address_germany_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Souffleur::Address::Germany do
-  it 'should return a dutch street' do
+  it 'should return a german street' do
     expect(Souffleur::Address::Germany::STREETS).to include(Souffleur::Address::Germany.street)
   end
 
