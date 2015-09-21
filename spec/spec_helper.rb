42530cb68be4d2e79d803bd2b9fd711f33359f74
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-09-21 21:53:46 +0200

    Add rspec matcher be_included_in

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'souffleur'
+
+RSpec::Matchers.define :be_included_in do |expected|
+  match do |actual|
+    expected.include?(actual)
+  end
+end
